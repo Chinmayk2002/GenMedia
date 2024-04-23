5dcb2bc53905d914412a80ddde5d7c4d82733f21
chinmaykamble <chinmaykamble@gmail.com>
2024-04-23 21:27:16 +0530

    img integration

--- client/src/pages/HomePage/Img.jsx
+++ client/src/pages/HomePage/Img.jsx
@@ -4,21 +4,26 @@ import axios from 'axios';
 function Img() {
   const [response, setResponse] = useState(null);
   const [error, setError] = useState(null);
-
+  const [imageURL, setImageURL] = useState('');
   const fetchData = async () => {
-    try {
-      const data = { prompt: "horse" };
+     try {
+      const data = {
+        prompt: "horse",
+      };
 
-      const response = await axios.post('https://41df58941b81c1855e.gradio.live/sdapi/v1/txt2img/', data, {
+      const response = await fetch('http://localhost:3001/api/forwardRequest', {
+        method: 'POST',
         headers: {
           'Content-Type': 'application/json'
-        }
+        },
+        body: JSON.stringify(data)
       });
 
-      setResponse(response.data);
+      const responseData = await response.json();
+      setResponse(responseData);
+      setImageURL(responseData.images);
     } catch (error) {
-      console.error('Error fetching data:', error);
-      setError(error.message); // Set error message to state for display
+      setError(error);
     }
   };
 
@@ -26,6 +31,7 @@ function Img() {
     <div>
       <button onClick={fetchData}>Fetch Data</button>
       {response && <pre>{JSON.stringify(response, null, 2)}</pre>}
+      {imageURL && <img src={imageURL} alt="Generated Image" />}
       {error && <p>Error: {error}</p>}
     </div>
   );
